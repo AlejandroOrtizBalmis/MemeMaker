50987e95b7634c2171519205b023c5596f756362
Alejandro Ortiz <alejandro.ortiz@iesdoctorbalmis.com>
2020-11-18 10:04:40 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace MemeMaker
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Model DataModel
+        {
+            get { return this.Resources["Foto"] as Model; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            BitmapEncoder encoder = new JpegBitmapEncoder();
+            //ImgFinal --> Grid aconvertir
+            RenderTargetBitmap rtb = this.DataModel.RenderVisaulToBitmap(grid, (int)grid.ActualWidth, (int)grid.ActualHeight);
+            MemoryStream file = new MemoryStream();
+            encoder.Frames.Add(BitmapFrame.Create(rtb));
+            encoder.Save(file);
+
+            if (file != null)
+            {
+                SaveFileDialog fdlg = new SaveFileDialog
+                {
+                    DefaultExt = "jpg",
+                    Title = "/datos",
+                    Filter = "*Jpeg files|.jpg|Bmp Files|*.bmp|PNG Files|*.png|Tiff Files|*.tif|Gif Files|*.gif"
+                };
+                bool? result = fdlg.ShowDialog();
+                if (result.HasValue && result.Value)
+                {
+                    using (FileStream fstream = File.OpenWrite(fdlg.FileName))
+                    {
+                        file.WriteTo(fstream);
+                        fstream.Flush();
+                        fstream.Close();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ MemeMaker/convertidorBorde.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class ConvertidorBorde : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value is true) return 3;
+            else return 0;
+
+        }
+
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
